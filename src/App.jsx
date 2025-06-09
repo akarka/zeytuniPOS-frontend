cb289e61b50afb31b31c7dd6a31b8ece525d6664
akarka <pkadir.akar@gmail.com>
2025-06-09 03:24:48 +0300

    sipariş onayla not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
-import UrunPage from "./pages/UrunPage";
-import SatisPage from "./pages/SatisPage";
-import TedarikciPage from "./pages/TedarikciPage";
-import LoginPage from "./pages/LoginPage";
-import AdminPage from "./pages/AdminPage";
-import RequireAuth from "./components/RequireAuth";
-import YonetimKontrolPage from "./pages/YonetimKontrolPage";
-import MasterLayout from "./components/MasterLayout";
+import UrunPage from './pages/UrunPage';
+import SatisPage from './pages/SatisPage';
+import TedarikciPage from './pages/TedarikciPage';
+import LoginPage from './pages/LoginPage';
+import AdminPage from './pages/AdminPage';
+import RequireAuth from './components/RequireAuth';
+import YonetimKontrolPage from './pages/YonetimKontrolPage';
+import SiparisPage from './pages/SiparisPage';
+import MasterLayout from './components/MasterLayout';
 
 function App() {
   <h1 className="text-3xl text-green-600 font-bold">Tailwind v3 aktif ✅</h1>;
 
   const [aktifKullanici, setAktifKullanici] = useState(
-    localStorage.getItem("aktifKullanici")
+    localStorage.getItem('aktifKullanici'),
   );
 
   useEffect(() => {
-    const kayitli = localStorage.getItem("aktifKullanici");
+    const kayitli = localStorage.getItem('aktifKullanici');
     if (kayitli) {
       setAktifKullanici(JSON.parse(kayitli));
     }
@@ -45,14 +46,37 @@ function App() {
           }
         >
           {/* Alt sayfalar MasterLayout içinde Outlet ile açılır */}
-          <Route index element={<AdminPage />} />
-          <Route path="urun" element={<UrunPage />} />
-          <Route path="satis" element={<SatisPage />} />
-          <Route path="tedarik" element={<TedarikciPage />} />
-          <Route path="admin/yonetimkontrol" element={<YonetimKontrolPage />} />
+          <Route
+            index
+            element={<AdminPage />}
+          />
+          <Route
+            path="urun"
+            element={<UrunPage />}
+          />
+          <Route
+            path="satis"
+            element={<SatisPage />}
+          />
+          <Route
+            path="siparis"
+            element={<SiparisPage />}
+          />
+          <Route
+            path="tedarik"
+            element={<TedarikciPage />}
+          />
+          <Route
+            path="admin/yonetimkontrol"
+            element={<YonetimKontrolPage />}
+          />
         </Route>
         {/* Fallback yönlendirme */}
-        <Route path="*" element={<Navigate to="/" />} />a
+        <Route
+          path="*"
+          element={<Navigate to="/" />}
+        />
+        a
       </Routes>
     </BrowserRouter>
   );
